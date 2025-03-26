c5b0b3cb2b24c7f31df7ca7809a6c1baf0f27455
Mark Johns <144073036+MarkBJohns@users.noreply.github.com>
2025-03-25 22:45:05 -0400

    Update upload-template.mjs

--- scripts/upload-template.mjs
+++ scripts/upload-template.mjs
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
-import { existsSync, rmSync, cpSync } from "fs";
+import { existsSync, mkdirSync, rmSync, cpSync } from "fs";
 import path from "path";
 import readline from "readline";
 import { fileURLToPath } from "url";
@@ -9,59 +9,34 @@ const __dirname = path.dirname(__filename);
 
 const args = process.argv.slice(2);
 const templatePath = args[0];
-const skipConfirm = args.includes("-y");
+const destination = args[1];
 
-if (!templatePath) {
-    console.error("Please provide a template name");
+if (!templatePath || !destination) {
+    console.error('Incorrect format, use: "npx template <template-path> <app-name>"');
     process.exit(1);
 }
 
 const source = path.resolve(__dirname, "../", templatePath);
-const target = process.cwd();
+const target = path.resolve(process.cwd(), destination);
+
+console.log("ARGS: ", args);
+console.log("templatePath: ", templatePath),
+console.log("destination: ", destination);
 
 if (!existsSync(source)) {
     console.error(`Template not found: ${source}`);
     process.exit(1);
 }
 
-const confirmAndRun = async () => {
-    if (skipConfirm) {
-        replaceDirectory();
-        return;
-        
-    }
-    
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    
-    rl.question(
-        `This will permanently delete ${target} and all of its contents. This cannot be undone. Continue? (y/n)`,
-        (answer) => {
-            rl.close();
-            
-            if (answer.toLowerCase() === "y") {
-                replaceDirectory();
-                
-            } else {
-                console.log("Process aborted");
-                process.exit(0);
-            }
-        }
-    )
+if (existsSync(target)) {
+    console.error(`Target folder already exists: ${target}`);
+    process.exit(1);
 }
 
-const replaceDirectory = () => {
-    console.log(`Replacing ${target} with ${templatePath}...`);
-    rmSync(target, { recursive: true, force: true });
-    cpSync(source, target, { recursive: true });
-    console.log("Template applied");
+mkdirSync(target, { recursive: true });
+cpSync(source, target, { recursive: true });
 
-    console.log(`\nTo re-enter the directory, type:`);
-    console.log(`$ cd ../${path.basename(target)}`);
-    console.log(`$ npm install`);
-    console.log(`$ npm run dev`);
-}
-
-await confirmAndRun();
+console.log("Template copied");
+console.log(`\nTo begin:`);
+console.log(`$ cd ${destination}`);
+console.log(`$ npm install`);
